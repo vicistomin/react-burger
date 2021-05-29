78708a6a3823559837406bca3e7ed701feb7de00
Vic Istomin <vic.istomin@gmail.com>
2021-05-29 10:53:18 +0300

    1st iteration of BurgerConstrustor

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -7,6 +7,11 @@ import AppHeader from '../app-header/app-header';
 import BurgerConstructor from '../burger-constructor/burger-constructor.jsx';
 import BurgerIngredients from '../burger-ingredients/burger-ingredients';
 
+// define arrays of ingredients from the test data:
+const topItem = testData[0];
+const middleItems = testData.slice(4, 12);
+const bottomItem = testData[0];
+
 function App() {
   return (
     <>
@@ -16,7 +21,7 @@ function App() {
           <BurgerIngredients items={testData}/>
         </section>
         <section className={appStyles.container_right + ' ml-5'}>
-          <BurgerConstructor items={testData} />
+          <BurgerConstructor topItem={topItem} middleItems={middleItems} bottomItem={bottomItem} />
         </section>
       </div>
     </>
--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -1,8 +1,58 @@
 import burgerConstructorStyles from './burger-constructor.module.css';
+// importing components from library
+import { ConstructorElement, DragIcon, CurrencyIcon, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
 function BurgerConstructor(props) {
     return(
-        null
+        <>
+            <ul className={burgerConstructorStyles.burger_constructor_list + ' ml-4 mt-25 mb-10 pr-4'}>
+                <li className='pl-8' key="top_bun">
+                    <ConstructorElement 
+                                    type='top'
+                                    isLocked={true}
+                                    text={props.topItem.name + ' (верх)'}
+                                    thumbnail={props.topItem.image}
+                                    price={props.topItem.price}
+                                />
+                </li>
+                <ul className={burgerConstructorStyles.burger_constructor_draggable_list + ' pr-2'} key="middle_items">
+                    {props.middleItems.map((item, index) => (
+                        <li key={item._id + '_' + index}>
+                            <span className={burgerConstructorStyles.burger_constructor_drag_icon}>
+                                <DragIcon type='primary' />
+                            </span>
+                            <ConstructorElement 
+                                text={item.name}
+                                thumbnail={item.image}
+                                price={item.price}
+                                />
+                        </li>
+                    ))}
+                </ul>
+                <li className='pl-8' key="bottom_bun">
+                    <ConstructorElement 
+                                    isLocked={true}
+                                    type='bottom'
+                                    text={props.bottomItem.name + ' (низ)'}
+                                    thumbnail={props.bottomItem.image}
+                                    price={props.bottomItem.price}
+                                />
+                </li>
+            </ul>
+            <div className={burgerConstructorStyles.burger_constructor_order + ' mb-10'}>
+                <p className="text text_type_digits-medium">
+                        {props.topItem.price + 
+                        props.middleItems.reduce((acc, p) => acc + p.price, 0) +
+                        props.bottomItem.price}
+                </p>
+                <span className='ml-2 mr-10'>
+                    <CurrencyIcon type="primary" />
+                </span>
+                <Button type="primary" size="medium">
+                        Оформить заказ
+                </Button>
+            </div>
+        </>
     );
 }
 
