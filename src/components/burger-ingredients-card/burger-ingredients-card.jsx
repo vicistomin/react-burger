9637f28f1df992e52874f5a5c61c1db411cb7595
Vic Istomin <vic.istomin@gmail.com>
2021-05-30 17:33:48 +0300

    Removed <a> tag in response to PR review

--- src/components/burger-ingredients-card/burger-ingredients-card.jsx
+++ src/components/burger-ingredients-card/burger-ingredients-card.jsx
@@ -6,18 +6,15 @@ import { Counter, CurrencyIcon } from '@ya.praktikum/react-developer-burger-ui-c
 function BurgerIngredientsCard(props) {
     return(
         <li className={burgerIngredientsCardStyles.ingredient_card}>
-            {/* <a> tag seems to be not the best choice for this (maybe <button> will be better?) */}
-            <a href='#' className={burgerIngredientsCardStyles.ingredient_card_link} title={props.name}>
-                {props.value ? <Counter count={props.value}/> : null}
-                <img src={props.image} alt={props.name} className="ml-4 mr-4"/>
-                    <div className={burgerIngredientsCardStyles.ingredient_price + ' mt-1 mb-1 '}>
-                        <p className='pr-2 text text_type_digits-default'>{props.price}</p>
-                        <CurrencyIcon />
-                    </div>
-                <p className={burgerIngredientsCardStyles.ingredient_name + ' text text_type_main-default'}>
-                    {props.name}
-                </p>
-            </a>
+            {props.value ? <Counter count={props.value}/> : null}
+            <img src={props.image} alt={props.name} title={props.name} className="ml-4 mr-4"/>
+                <div className={burgerIngredientsCardStyles.ingredient_price + ' mt-1 mb-1 '}>
+                    <p className='pr-2 text text_type_digits-default'>{props.price}</p>
+                    <CurrencyIcon />
+                </div>
+            <p className={burgerIngredientsCardStyles.ingredient_name + ' text text_type_main-default'}>
+                {props.name}
+            </p>
         </li>
     );
 }
