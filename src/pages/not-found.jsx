06521f575c6271aa49e351df7ba6f131d9dd008b
Vic Istomin <vic.istomin@gmail.com>
2021-07-08 23:23:04 +0300

    feat: new 404 page

--- src/pages/not-found.jsx
+++ src/pages/not-found.jsx
@@ -1,8 +1,20 @@
 import styles from './not-found.module.css';
+// importing components from project
+import AppHeader from '../components/app-header/app-header';
 
 export const NotFound404 = () => {
   return(
-    null
+    <>
+      <AppHeader />
+      <div className={styles.fullscreen_message}>
+        <p className='text text_type_digits-large text_color_inactive'>
+          404
+        </p>
+        <h1 className='text text_type_main-large text_color_inactive'>
+          Страница не найдена
+        </h1>
+      </div>
+    </>
   );
 }
 
