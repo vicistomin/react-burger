9507731d61f614ff22fa7816fc1f95dc51e6e323
Vic Istomin <vic.istomin@gmail.com>
2021-07-08 20:21:54 +0300

    feat: added new API urls for auth

--- src/services/constants.jsx
+++ src/services/constants.jsx
@@ -1,2 +1,9 @@
 export const ITEMS_API_URL = 'https://norma.nomoreparties.space/api/ingredients';
 export const ORDER_API_URL = 'https://norma.nomoreparties.space/api/orders';
+export const FORGOT_PASSWORD_API_URL = 'https://norma.nomoreparties.space/api/password-reset';
+export const RESET_PASSWORD_API_URL = 'https://norma.nomoreparties.space/api/password-reset/reset';
+export const REGISTER_API_URL = 'https://norma.nomoreparties.space/api/auth/register';
+export const LOGIN_API_URL = 'https://norma.nomoreparties.space/api/auth/login';
+export const LOGOUT_API_URL = 'https://norma.nomoreparties.space/api/auth/logout';
+export const TOKEN_API_URL = 'https://norma.nomoreparties.space/api/auth/token';
+export const USER_API_URL = 'https://norma.nomoreparties.space/api/auth/user';
