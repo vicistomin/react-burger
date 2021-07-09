8fba4d192dd47a4f55cbf16b0695d8d961351c03
Vic Istomin <vic.istomin@gmail.com>
2021-07-09 18:52:18 +0300

    feat: 1st iteration of Login page

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,8 +1,47 @@
+import React from 'react';
 import styles from './login.module.css';
+// importing components from project
+import AppHeader from '../components/app-header/app-header';
+import Form from '../components/form/form';
+
+import { Input, PasswordInput } from '@ya.praktikum/react-developer-burger-ui-components';
 
 export const LoginPage = () => {
+
+  const [value, setValue] = React.useState('');
+  const inputRef = React.useRef(null);
+  const onChange = e => {
+    setValue(e.target.value)
+  };
+
   return(
-    null
+    <>
+      <AppHeader />
+      <div className={styles.fullscreen_message}>
+        <Form
+          title='Вход'
+          actionName='Войти'
+        >
+          {/* TODO: Implement email validation on the fly */}
+          <Input
+            type={'email'}
+            placeholder={'E-mail'}
+            onChange={e => setValue(e.target.value)}
+            value={value}
+            name={'email'}
+            error={false}
+            ref={inputRef}
+            errorText={'Ошибка'}
+            size={'default'}
+          />
+          <PasswordInput
+            onChange={onChange}
+            value={value}
+            name={'password'}
+          />
+        </Form>
+      </div>
+    </>
   );
 }
 
