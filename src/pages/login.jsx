c8c4a1e427f6965fe1395d8cbb65e07d523ffb1f
Vic Istomin <vic.istomin@gmail.com>
2021-07-10 16:48:57 +0300

    feat: validation and fake auth in LoginPage

--- src/pages/login.jsx
+++ src/pages/login.jsx
@@ -1,7 +1,9 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 // importing components from project
 import AppHeader from '../components/app-header/app-header';
 import Form from '../components/form/form';
+import Loader from '../components/loader/loader';
+import { fakeAuth } from '../services/auth';
 
 import { Input, PasswordInput, Button } from '@ya.praktikum/react-developer-burger-ui-components';
 
@@ -11,29 +13,85 @@ export const LoginPage = () => {
 
   const history = useHistory();
 
+  const [isFormProcessing, setFormProcessing] = useState(false);
+
   const [emailValue, setEmailValue] = useState('');
   const [isEmailValid, setEmailValid] = useState(true);
   const [passwordValue, setPasswordValue] = useState('');
+  const [isPasswordEmpty, setPasswordEmpty] = useState(false);
 
   const emailInputRef = useRef(null);
 
   const emailRegExp = /.+@.+\.[A-Za-z]+$/;
 
   const onEmailChange = e => {
+    // hide the error message if user writed correct email in the field
+    if (emailRegExp.test(e.target.value)) {
+      setEmailValid(true);
+    }
     setEmailValue(e.target.value);
   };
   
   const onPasswordChange = e => {
+    // TODO: find a way to trigger PasswordInput error status
+    if (e.target.value.length !== 0) {
+      setPasswordEmpty(false);
+    }
     setPasswordValue(e.target.value);
   };
 
-  const onLoginClick = () => {
+  // TODO: move form/inputs validation function to separate file in /utils?
+
+  const validateForm = () => {
+    // TODO: check is better be done when focus is out of input, before the form submit action
+  
+    const validFields = {
+      email: false,
+      password: false
+    }
+    
     // performing email field check
-    // TODO: check are better be done when focus is out of input, before the form submit action
     if (!emailRegExp.test(emailValue)) {
       setEmailValid(false);
     }
-    // TODO: implement login action
+    else {
+      validFields.email = true;
+    }
+
+    // performing password field check    
+    if (passwordValue.length < 6) {
+      // TODO: find a way to trigger PasswordInput error status
+      if (passwordValue.length === 0) {
+        setPasswordEmpty(true);
+      }
+    }
+    else {
+      validFields.password = true;
+    }
+
+    if (validFields.email && validFields.password) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  const onLoginClick = async () => {
+    const isFormCorrect = validateForm();
+    if(!isFormCorrect) {
+      return;
+    }
+    else {
+      // if form fields are correct then start network request
+      setFormProcessing(true);
+      // TODO: implement login action
+      const success = await fakeAuth();
+      setFormProcessing(false);
+      if(success) {
+        history.replace({ pathname: '/' });
+      }
+    }
   }
 
   const onRegisterClick = () => {
@@ -44,16 +102,10 @@ export const LoginPage = () => {
     history.replace({ pathname: '/forgot-password' });
   }
 
-  useEffect(() => {
-    // hide the error message if user writed correct email in the field
-    if (emailRegExp.test(emailValue)) {
-      setEmailValid(true);
-    }
-  }, [emailValue]);
-
   return(
     <>
       <AppHeader />
+      {isFormProcessing && <Loader />}
       <div className='fullscreen_message'>
         <Form
           title='Вход'
@@ -71,11 +123,13 @@ export const LoginPage = () => {
             errorText={'Неправильно введен e-mail'}
             size={'default'}
           />
-          <PasswordInput
-            onChange={onPasswordChange}
-            value={passwordValue}
-            name={'password'}
-          />
+          <div className={isPasswordEmpty ? 'password_error' : ''}>
+            <PasswordInput
+              onChange={onPasswordChange}
+              value={passwordValue}
+              name={'password'}
+            />
+          </div>
         </Form>
         <div className='bottom_navigation'>
           <p className="text text_type_main-default text_color_inactive">
