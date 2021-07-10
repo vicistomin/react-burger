b967eb79f5be6a893442c5bcde5b94ac43b636dd
Vic Istomin <vic.istomin@gmail.com>
2021-07-10 19:07:29 +0300

    feat: activate nav links in AppHeader by url check

--- src/components/app-header/app-header.jsx
+++ src/components/app-header/app-header.jsx
@@ -5,10 +5,32 @@ import MenuItem from '../menu-item/menu-item';
 import { Logo, BurgerIcon, ListIcon, ProfileIcon } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import { useHistory } from 'react-router-dom';
+import { useState, useEffect } from 'react';
 
 function AppHeader() {
-
     const history = useHistory();
+
+    const [isHomePage, setHomePage] = useState(false);
+    const [isFeedPage, setFeedPage] = useState(false);
+    const [isProfilePage, setProfilePage] = useState(false);
+
+    const currentUrl = history.location.pathname;
+    
+    useEffect(() => {
+        switch (currentUrl) {
+            case '/':
+                setHomePage(true);
+                break;
+            case '/feed':
+                setFeedPage(true);
+                break;
+            case '/profile':
+                setProfilePage(true);
+                break;
+            default:
+                break;
+        }
+    }, [currentUrl]);
 
     const onConstructorClick = () => {
         history.replace({ pathname: '/' });
@@ -27,25 +49,25 @@ function AppHeader() {
                 <ul className={appHeaderStyles.menu_list}>
                     <li className={appHeaderStyles.menu_list_left}>
                         <ul className={appHeaderStyles.menu_list_left_items}>
-                            {/* TODO: reimplement active/hover/inactive link and icon colors */}
                             <li>
                                 <MenuItem
                                     icon={
-                                        <BurgerIcon type="primary" />
+                                        <BurgerIcon type={isHomePage ? "primary" : "secondary"} />
                                     }
                                     text="Конструктор"
                                     onClick={onConstructorClick}
-                                    active
+                                    active={isHomePage}
                                 />
                             </li>
                             <li>
 
                                 <MenuItem
                                     icon={
-                                        <ListIcon type="secondary" />
+                                        <ListIcon type={isFeedPage ? "primary" : "secondary"} />
                                     }
                                     text="Лента заказов"
                                     onClick={onFeedClick}
+                                    active={isFeedPage}
                                 />
                             </li>
                         </ul>
@@ -57,10 +79,11 @@ function AppHeader() {
                         <span>
                             <MenuItem
                                 icon={
-                                    <ProfileIcon type="secondary" />
+                                    <ProfileIcon type={isProfilePage ? "primary" : "secondary"} />
                                 }
                                 text="Личный кабинет"
                                 onClick={onProfileClick}
+                                active={isProfilePage}
                             />
                         </span>
                     </li>
