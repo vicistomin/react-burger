3aa11866c25c70a51940f65806fb0943808c6c68
Vic Istomin <vic.istomin@gmail.com>
2021-07-14 18:32:28 +0300

    feat: 2nd iteration of ProfilePage

--- src/pages/profile.jsx
+++ src/pages/profile.jsx
@@ -3,40 +3,74 @@ import styles from './profile.module.css';
 import AppHeader from '../components/app-header/app-header';
 import Form from '../components/form/form';
 import Sidebar from '../components/sidebar/sidebar';
-import { Input } from '@ya.praktikum/react-developer-burger-ui-components';
+import { Input, EmailInput } from '@ya.praktikum/react-developer-burger-ui-components';
 
 import { useState, useRef } from 'react';
+import { fakeUserData } from "../services/user-data";
 
 export const ProfilePage = () => {
-  const [value, setValue] = useState('Марк')
-  const [isInputDisabled, setInputDisabled] = useState(true)
-  const [isInputEmpty, setInputEmpty] = useState(false)
+  // TODO: create EditableInput component and move there all this checks and handlers 
+  // TODO: move data to slices and operate through dispatches
+  const [nameValue, setNameValue] = useState(fakeUserData.name)
+  const [emailValue, setEmailValue] = useState(fakeUserData.email)
+  const [passwordValue, setPasswordValue] = useState(fakeUserData.password)
+  const [isNameInputDisabled, setNameInputDisabled] = useState(true)
+  const [isNameInputEmpty, setNameInputEmpty] = useState(false)
+  const [isPasswordInputDisabled, setPasswordInputDisabled] = useState(true)
+  const [isPasswordInputEmpty, setPasswordInputEmpty] = useState(false)
 
-  const inputRef = useRef(null)
+  const nameInputRef = useRef(null)
+  const passwordInputRef = useRef(null)
 
-  const onInputChange = e => {
+  const onNameChange = e => {
     // hide the error message if user is writing something in the name field
     if (e.target.value.length > 0) {
-      setInputEmpty(false);
+      setNameInputEmpty(false);
     }
-    setValue(e.target.value);
+    setNameValue(e.target.value);
+    fakeUserData.name = e.target.value;
   };
 
-  const onIconClick = () => {
-    inputRef.current.focus();
-    setInputDisabled(false);
+  const onPasswordChange = e => {
+    // hide the error message if user is writing something in the password field
+    if (e.target.value.length > 0) {
+      setPasswordInputEmpty(false);
+    }
+    setPasswordValue(e.target.value);
+    fakeUserData.password = e.target.value;
+  };
+
+  const onEmailChange = e => {
+    // hide the error message if user is writing something in the password field
+    setEmailValue(e.target.value);
+    fakeUserData.email = e.target.value;
+  };
+
+  const onNameIconClick = () => {
+    nameInputRef.current.focus();
+    setNameInputDisabled(false);
   }
 
-  const onInputBlur = e => {
+  const onPasswordIconClick = () => {
+    passwordInputRef.current.focus();
+    setPasswordInputDisabled(false);
+  }
+
+  const onNameInputBlur = e => {
     // show the error message if the name field is blank
     if (e.target.value.length === 0) {
-      setInputEmpty(true);
+      setNameInputEmpty(true);
     }
-    setInputDisabled(true);
+    setNameInputDisabled(true);
   }
 
-
-
+  const onPasswordInputBlur = e => {
+    // show the error message if the password field is blank
+    if (e.target.value.length === 0) {
+      setPasswordInputEmpty(true);
+    }
+    setPasswordInputDisabled(true);
+  }
 
   return(
     <>
@@ -48,40 +82,39 @@ export const ProfilePage = () => {
             <Input
               type={'text'}
               placeholder={'Имя'}
-              onChange={onInputChange}
-              value={value}
+              onChange={onNameChange}
+              value={nameValue}
               name={'name'}
-              error={isInputEmpty}
-              ref={inputRef}
+              error={isNameInputEmpty}
+              ref={nameInputRef}
               errorText={'Поле не может быть пустым'}
               size={'default'}
               icon={'EditIcon'}
-              onIconClick={onIconClick}
-              disabled={isInputDisabled}
-              onBlur={onInputBlur}
+              onIconClick={onNameIconClick}
+              disabled={isNameInputDisabled}
+              onBlur={onNameInputBlur}
             />
-            {/* <Input
-              type={'text'}
-              placeholder={'Логин'}
-              // onChange={onNameChange}
-              // value={nameValue}
-              name={'name'}
-              // error={isNameEmpty}
-              // ref={nameInputRef}
-              errorText={'Поле не может быть пустым'}
+            <EmailInput
+              onChange={onEmailChange}
+              value={emailValue}
+              name={'email'}
               size={'default'}
-            />
+            />            
             <Input
               type={'text'}
               placeholder={'Пароль'}
-              // onChange={onNameChange}
-              // value={nameValue}
-              name={'name'}
-              // error={isNameEmpty}
-              // ref={nameInputRef}
+              onChange={onPasswordChange}
+              value={passwordValue}
+              name={'password'}
+              error={isPasswordInputEmpty}
+              ref={passwordInputRef}
               errorText={'Поле не может быть пустым'}
               size={'default'}
-            /> */}
+              icon={'EditIcon'}
+              onIconClick={onPasswordIconClick}
+              disabled={isPasswordInputDisabled}
+              onBlur={onPasswordInputBlur}
+            />
           </Form>
         </div>
       </div>
