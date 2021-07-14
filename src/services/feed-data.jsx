26405f86102526c002dbcc6290a3b0fc4a5bcddf
Vic Istomin <vic.istomin@gmail.com>
2021-07-14 18:27:22 +0300

    feat: "fake api" for hardcoded data

---
+++ src/services/feed-data.jsx
@@ -0,0 +1,58 @@
+export const fakeFeedData = {
+  orders: [
+    {
+      id: '034536',
+      type: 'Death Star Starship Main бургер',
+      status: 'Создан',
+      time: 1626015158221,
+      price: 480,
+      ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733cb',
+        '60d3b41abdacab0026a733cc',
+        '60d3b41abdacab0026a733d4'
+      ]
+    },
+    {
+      id: '034535',
+      type: 'Death Main бургер',
+      status: 'Готовится',
+      time: 1626015158021,
+      price: 1420,
+      ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733cb',
+        '60d3b41abdacab0026a733cc',
+        '60d3b41abdacab0026a733d1',
+        '60d3b41abdacab0026a733d2',
+        '60d3b41abdacab0026a733cd',
+        '60d3b41abdacab0026a733c8',
+        '60d3b41abdacab0026a733c9'
+      ]
+    },
+    {
+      id: '034534',
+      type: 'Death Star Main бургер',
+      status: 'Выполнен',
+      time: 1626015118221,
+      price: 510,
+      ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733cc',
+        '60d3b41abdacab0026a733d4'
+      ]
+    },
+    {
+      id: '034533',
+      type: 'Star Main бургер',
+      status: 'Отменён',
+      time: 1626014158221,
+      price: 310,
+      ingredients: [
+        '60d3b41abdacab0026a733c6',
+        '60d3b41abdacab0026a733cc',
+        '60d3b41abdacab0026a733d4'
+      ]
+    }
+  ]
+};
