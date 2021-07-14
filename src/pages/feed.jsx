3ba3c42ebf423448a7109775eec45d2caf04648d
Vic Istomin <vic.istomin@gmail.com>
2021-07-14 18:33:30 +0300

    feat: blank template for FeedPage

--- src/pages/feed.jsx
+++ src/pages/feed.jsx
@@ -1,8 +1,14 @@
 import styles from './feed.module.css';
+// importing components from project
+import AppHeader from '../components/app-header/app-header';
 
 export const FeedPage = () => {
   return(
-    null
+    <>
+      <AppHeader />
+
+
+    </>
   );
 }
 
