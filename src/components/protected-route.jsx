5d5861857f5f9918377fd79ee8edd5a2d2d8be51
Vic Istomin <vic.istomin@gmail.com>
2021-07-14 18:33:50 +0300

    feat: template for ProtectedRoute

---
+++ src/components/protected-route.jsx
@@ -0,0 +1,41 @@
+import { useAuth } from '../services/auth';
+import { Redirect, Route } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+// TODO: implement protection for route /reset-password
+
+export function ProtectedRoute({ children, ...rest }) {
+  let { getUser, ...auth } = useAuth();
+  const [isUserLoaded, setUserLoaded] = useState(false);
+
+  const init = async () => {
+    await getUser();
+    setUserLoaded(true);
+  };
+
+  useEffect(() => {
+    init();
+  }, []);
+
+  if (!isUserLoaded) {
+    return null;
+  }
+
+  return (
+    <Route
+      {...rest}
+      render={({ location }) =>
+        auth.user ? (
+          children
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
