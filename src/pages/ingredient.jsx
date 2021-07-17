5f43216775b43568b9dd3df9d3623cd3cada716e
Vic Istomin <vic.istomin@gmail.com>
2021-07-17 23:47:41 +0300

    feat: 1st iteration of IngredientPage

--- src/pages/ingredient.jsx
+++ src/pages/ingredient.jsx
@@ -1,8 +1,64 @@
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import styles from './ingredient.module.css';
+// importing components from project
+import AppHeader from '../components/app-header/app-header';
+import IngredientDetails from '../components/ingredient-details/ingredient-details';
+import Loader from '../components/loader/loader';
+// import slices and their functions
+import { getItems } from '../services/slices/items';
 
 export const IngredientPage = () => {
-  return(
-    null
+  const dispatch = useDispatch();
+  
+  const {
+    items,
+    itemsRequest,
+    itemsSuccess,
+    itemsFailed
+  } = useSelector(
+    state => state.items
+  );
+
+  // we need to have items from API in store to render ingredient
+  useEffect(() => {
+    // won't call API if items are already in store
+    if (!itemsSuccess) {
+      dispatch(getItems());
+    }
+  }, [dispatch, itemsSuccess]);
+
+  const currentIngredientId = useParams().id;
+
+  return (
+    <>
+    <AppHeader />
+    {
+      itemsRequest && 
+      !itemsFailed && 
+      !itemsSuccess && (
+        <Loader />
+    )}
+    {
+      itemsFailed && 
+      !itemsRequest && 
+      !itemsSuccess && (
+        <h2 className='fullscreen_message text text_type_main-large text_color_inactive'>
+          Ошибка загрузки
+        </h2>
+    )}
+    {
+      itemsSuccess && 
+      !itemsFailed && 
+      !itemsRequest && (
+        <div className='fullscreen_message'>
+          <IngredientDetails
+            item={items.find((item) => item._id === currentIngredientId)}
+          />
+        </div>
+      )}
+  </>
   );
 }
 
