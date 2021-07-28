86ab673967ea48d9de8ed187664cc2b6561559ea
Vic Istomin <vic.istomin@gmail.com>
2021-07-28 16:33:49 +0300

    feat: passing tests for BurgerCorstructorSlice

--- src/services/slices/burger-constructor.test.js
+++ src/services/slices/burger-constructor.test.js
@@ -1,11 +1,16 @@
 import { burgerConstructorSlice } from "./burger-constructor";
 import configureMockStore from 'redux-mock-store'
-import rootReducer from './services/slices';
+import rootReducer from './';
 import thunk from 'redux-thunk';
 
 const testBunItem = {
   _id: 123,
   name: "Тестовая булка"
+};
+
+const testMiddleItem = {
+  _id: 456,
+  name: "Тестовый ингредиент"
 };
 
 const initStore = {
@@ -14,29 +19,64 @@ const initStore = {
   totalPrice: 0
 }
 
+const storeWithMiddleItem = {
+  ...initStore,
+  middleItems: [testMiddleItem]
+}
+
 const storeWithBun = {
   ...initStore,
   bunItem: testBunItem
 }
 
+const {
+  addMiddleItem,
+  clearMiddleItems,
+  setBunItem
+} = burgerConstructorSlice.actions
 
-const { reducer } = burgerConstructorSlice;
-
-describe('tests for burgerConstructor reducers', () => {
+describe('tests for burgerConstructorSlice reducers', () => {
 
   const mockStore = configureMockStore({
     reducer: rootReducer,
     middleware: [thunk]
   })
+  const store = mockStore(initStore)
 
   it('should return the initial state', () => {
-    expect(reducer(undefined, {}))
+    expect(burgerConstructorSlice.reducer(undefined, {}))
     .toEqual(initStore)
   })
 
-  it('should add the bun in the store', () => {
-    const {getState} = mockStore
-    expect(getState.dispatch.setBunItem(testBunItem))
+  it('should add the middle item', () => {
+    expect(burgerConstructorSlice.reducer(
+      store.getState(),
+      addMiddleItem(testMiddleItem)
+    )) 
+    .toEqual(storeWithMiddleItem)
+  })
+
+  it('should clear the middle items', () => {
+    expect(burgerConstructorSlice.reducer(
+      store.getState(),
+      clearMiddleItems()
+    )) 
+    .toEqual(initStore)
+  })
+
+  it('should add the bun', () => {
+    expect(burgerConstructorSlice.reducer(
+      store.getState(),
+      setBunItem(testBunItem)
+    ))
     .toEqual(storeWithBun)
+  })
+
+  it('should remove the bun', () => {
+    expect(burgerConstructorSlice.reducer(
+      store.getState(),
+      setBunItem({})
+    ))
+    .toEqual(initStore)
   })
 })
