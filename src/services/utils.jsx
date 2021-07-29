0d44a3e23532fa26edace52c9609609e2613ac32
Vic Istomin <vic.istomin@gmail.com>
2021-07-29 23:25:51 +0300

    fix: removed unused RegExp rules in expression

--- src/services/utils.jsx
+++ src/services/utils.jsx
@@ -29,7 +29,7 @@ export const formatDateTime = (time) => {
 
 export function getCookie(name) {
   const matches = document.cookie.match(
-    new RegExp('(?:^|; )' + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + '=([^;]*)')
+    new RegExp('(?:^|; )' + name.replace(/([$?*|{}\]\\^])/g, '\\$1') + '=([^;]*)')
   );
   return matches ? decodeURIComponent(matches[1]) : undefined;
 }
