87780a429e09209317d41af8a84d2d3f107d40ad
Vic Istomin <vic.istomin@gmail.com>
2021-07-31 00:01:38 +0300

    feat: add functional Cypress tests for drag'n'drop

--- cypress/integration/constructor.spec.js
+++
@@ -1,9 +0,0 @@
-describe('constructor page functional test', function() {
-  it('should be available on localhost:3000', function() {
-    cy.visit('http://localhost:3000');
-  });
-  it('should drag and drop', function() {
-    cy.get('[class^=burger-ingredients-card_ingredient_card__]').first().trigger('dragstart');
-    cy.get('[class^=burger-constructor_emptyBun__]').first().trigger('drop');
-  });
-}); 
